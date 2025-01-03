2f1de63f84393a0ad2f9c040d104d61fa56f8c0a
namaksimow <144584830+namaksimow@users.noreply.github.com>
2025-01-03 05:06:22 +0400

    2 commit, clear the Program file, add project Domain, add domain model Song

---
+++ Domain/Models/Song.cs
@@ -0,0 +1,33 @@
+﻿namespace Domain.Models;
+
+public class Song
+{
+    private const int MAX_TITLE_LENGTH = 255;
+    
+    public Guid Id { get; }
+    
+    public string Title { get; } = string.Empty;
+    
+    public int Duration { get; }
+
+    private Song(Guid id, string title, int duration)
+    {
+        Id = id;
+        Title = title;
+        Duration = duration;
+    }
+
+    public static (Song Song, string Error) Create(Guid id, string title, int duration)
+    {
+        var error = String.Empty;
+
+        if (string.IsNullOrEmpty(title) || title.Length > MAX_TITLE_LENGTH)
+        {
+            error = "Invalid title";
+        }
+        
+        var song = new Song(id, title, duration);
+        
+        return (song, error);
+    }
+}
